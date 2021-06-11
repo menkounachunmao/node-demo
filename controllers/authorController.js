5cdf4a64e3b4bc43695f824f6a0de80f7c9109f1
Qing <1187989069@qq.com>
2021-06-11 16:24:35 +0800

    更新作者信息

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -2,7 +2,7 @@
  * @Author: xx
  * @Date: 2021-06-03 16:35:08
  * @LastEditors: 青峰
- * @LastEditTime: 2021-06-11 16:14:20
+ * @LastEditTime: 2021-06-11 16:23:03
  * @FilePath: /helloworld/controllers/authorController.js
  */
 
@@ -153,4 +153,36 @@ exports.author_update_get = (req, res, next) => {
  };
 
 // 由 POST 处理作者更新操作
-exports.author_update_post = (req, res) => { res.send('未实现：更新作者的 POST'); };
+exports.author_update_post = [
+    body('first_name').isLength({ min:1 }).trim().withMessage('First name must be specified. ')
+    .isAlphanumeric().withMessage('First name ha non-alphanumeric characters. '),
+    body('family_name').isLength({ min:1 }).trim().withMessage('Family name must be specified. ')
+    .isAlphanumeric().withMessage('Family name ha non-alphanumeric characters. '),
+    body('date_of_birth', 'Invalid date of birth').optional({ checkFalsy: true}).isISO8601(),
+    body('date_of_death', 'Invalid date of death').optional({ checkFalsy: true}).isISO8601(),
+
+    sanitizeBody('first_name').trim().escape(),
+    sanitizeBody('family_name').trim().escape(),
+    sanitizeBody('date_of_birth').toDate(),
+    sanitizeBody('date_of_death').toDate(),
+    (req, res, next)=>{
+        const errors = validationResult(req);
+        if(!errors.isEmpty()){
+            res.render('author_form',{ title: 'Create Author', author: req.body, errors: errors.array()});
+            return;
+        }
+        let author = new Author({
+            _id: req.params.id,
+            first_name: req.body.first_name,
+            family_name: req.body.family_name,
+            date_of_birth: req.body.date_of_birth,
+            date_of_death: req.body.date_of_death
+        })
+        Author.findByIdAndUpdate(req.params.id,author,{},function(err){
+            if(err){
+                return next(err);
+            }
+            res.redirect(author.url)
+        })
+    }
+];
